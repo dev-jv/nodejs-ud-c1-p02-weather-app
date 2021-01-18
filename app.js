b67b98164778355a0f5d6f556d388041e1130769
dev-jv <dev.javierleon@gmail.com>
2021-01-18 11:05:16 -0500

    two

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direction: {
         alias: 'd',
@@ -7,4 +9,18 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direction);
+// console.log(argv.direction);
+const encodedUrl = encodeURI(argv.direction);
+// console.log(encodedUrl);
+
+const instance = axios.create({
+    baseURL: `https://api.openweathermap.org/data/2.5/weather?q=${encodedUrl}&appid=c26c24f7c4c1ac550e7c5e6a0e8810ae`,
+});
+
+instance.get()
+    .then( resp => {
+        console.log(resp.data);
+    })
+    .catch( err => {
+        console.log('error!!!!!!!!!', err)
+    });
