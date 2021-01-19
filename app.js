3530aac066b90ecf5ce1c876619b3fedb5e25ca8
dev-jv <dev.javierleon@gmail.com>
2021-01-19 11:43:26 -0500

    five

--- app.js
+++ app.js
@@ -1,53 +1,137 @@
+// --------------------------------------- <> ['location' / 'weather'] > [lat-lon / temp]
+// const plc = require('./location/location');
+// const wht = require('./weather/weather');
+//
+// const argv = require('yargs')
+//
+//     .command('location', '...', {
+//         direction: {
+//             alias: 'd',
+//             desc: 'Ciudad de búsqueda',
+//             demand: true
+//         }
+//     })
+//     .command('weather', '....', {
+//         latitude: {
+//             alias: 't',
+//             desc: 'latitud',
+//             demand: true
+//         },
+//         longitude: {
+//             alias: 'g',
+//             desc: 'longitud',
+//             demand: true
+//         }
+//     })
+//     .help()
+//     .argv;
+//
+// let comando = argv._[0];
+//
+// switch (comando) {
+//     case "location":
+//         plc.getLocationLatLon(argv.direction)
+//             .then(x => {
+//                 console.log(x);
+//             })
+//             .catch(e => {
+//                 console.log(e)
+//             });
+//         break;
+//     case "weather":
+//         wht.getWeather(argv.latitude, argv.longitude)
+//             .then(x => {
+//                 console.log(x);
+//             })
+//             .catch(e => {
+//                 console.log(e)
+//             });
+//         break;
+//     default:
+//         console.log("El comando no es correecto!")
+// }
+
+// --------------------------------------- <> [latitude - Longitude] > [Temperature]
+// const plc = require('./location/location');
+// const wht = require('./weather/weather');
+//
+// const argv = require('yargs').options({
+//     // direction: {
+//     //     alias: 'd',
+//     //     desc: 'Ciudad de búsqueda',
+//     //     demand: true
+//     // },
+//     latitude: {
+//         alias: 't',
+//         desc: 'latitud',
+//         demand: true
+//     },
+//     longitude: {
+//         alias: 'g',
+//         desc: 'longitud',
+//         demand: true
+//     },
+// }).argv;
+//
+//
+// // plc.getLocationLatLon(argv.direction)
+// //     .then(x => {
+// //         console.log(x);
+// //     })
+// //     .catch(e => {
+// //         console.log(e)
+// //     });
+//
+// wht.getWeather(argv.latitude, argv.longitude)
+//     .then(x => {
+//         console.log(x);
+//     })
+//     .catch(e => {
+//         console.log(e)
+//     });
+
+// --------------------------------------- <> [City] > [Temperature]
 const plc = require('./location/location');
 const wht = require('./weather/weather');
 
-const argv = require('yargs')
+const colors = require('colors');
 
-    .command('location', '...', {
-        direction: {
-            alias: 'd',
-            desc: 'Ciudad de búsqueda',
-            demand: true
-        }
-    })
-    .command('weather', '....', {
-        latitude: {
-            alias: 't',
-            desc: 'latitud',
-            demand: true
-        },
-        longitude: {
-            alias: 'g',
-            desc: 'longitud',
-            demand: true
-        }
-    })
-    .help()
-    .argv;
+const argv = require('yargs').options({
+    direction: {
+        alias: 'd',
+        desc: 'Ciudad de búsqueda',
+        demand: true
+    }
+}).argv;
 
-let comando = argv._[0];
-
-switch (comando) {
-    case "location":
-        plc.getLocationLatLon(argv.direction)
-            .then(x => {
-                    console.log(x);
-                }
-            )
+const getInfo = async(dir) => {
+    try{
+        const coords = await plc.getLocationLatLon(dir)
+            // .then(x => {
+            //     console.log(x);
+            // })
             .catch(e => {
                 console.log(e)
             });
-        break;
-    case "weather":
-        wht.getWeather(argv.latitude, argv.longitude)
-            .then(x => {
-                    console.log(x);
-                }
-            )
+        const temp = await wht.getWeather(coords.lat, coords.lng)
+            // .then(x => {
+            //     console.log(x);
+            // })
             .catch(e => {
                 console.log(e)
             });
-        break;
-    default:
-        console.log("El comando no es correecto!")
-}
+        return (
+            `   ----------------------------- // -------------\n - The weather in ${coords.addr} has a ${temp.tAct}° of temperature`.blue
+        )
+    } catch (e) {
+        return ` - No data could be determined for ${dir}`.red
+    }
+};
+
+getInfo(argv.direction)
+    .then(x => {
+        console.log(x);
+    })
+    .catch(e => {
+        console.log(e)
+    });
--- weather/weather.js
+++ weather/weather.js
@@ -2,11 +2,10 @@ const axios = require('axios');
 
 const getWeather  = async (lat, lon) => {
 
-    // const encodedUrl = encodeURI(address);
     let data;
     try {
         const instance = axios.create({
-            baseURL: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c26c24f7c4c1ac550e7c5e6a0e8810ae`,
+            baseURL: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c26c24f7c4c1ac550e7c5e6a0e8810ae&units=metric`,
         });
         const resp = await instance.get();
         data = resp.data;
@@ -15,12 +14,12 @@ const getWeather  = async (lat, lon) => {
             throw new Error(`No hay resultados para lat = ${lat} & lon = ${lon}`);
         }
     }
-    const addr = data.name;
+    const tAct = data.main.temp;
     const tMax = data.main.temp_max;
     const tMin = data.main.temp_min;
 
     return {
-        addr,
+        tAct,
         tMax,
         tMin
     }
